d557af594ff7f7f8d6eca605e91cae0cfd1d88c4
Edgar Gonzalez <edgar.gonzalez@beracahmedica.mx>
2021-06-04 13:44:44 -0700

    VPC

---
+++ network/vpc/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  type        = string
+}
+
+variable "environment" {
+  description = "VPC description"
+  type        = string
+}
