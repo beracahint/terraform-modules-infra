6b8249c0d69db2bf788ee0100d2ed25b264f96c6
Edgar Gonzalez <edgar.gonzalez@beracahmedica.mx>
2021-07-11 22:51:27 -0700

    SP added

---
+++ storage/sp/variables.tf
@@ -0,0 +1,22 @@
+variable allocated_storage {}
+variable instance_class {}
+variable db_name {
+  type = string
+}
+variable vpc_id {}
+variable security_groups {
+  type = list(string)
+}
+variable snapshot_arn {}
+variable region {}
+variable environment {}
+variable subnet_ids_group {
+  type = list(string)
+}
+variable engine {}
+variable skip_final_snapshot {
+  type = bool
+}
+variable publicly_accessible {
+  type = bool
+}
